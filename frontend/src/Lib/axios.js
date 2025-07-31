4d5b6a31a0727ba37725bd30f3b6e17e1bd9e044
anoop-unq <padakantianoop7@gmail.com>
2025-08-01 00:56:24 +0530

    Axios

--- frontend/src/Lib/axios.js
+++ frontend/src/Lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const BASE_URL = 'https://task-manager-mern-app-dj9e.onrender.com/api/notes';
-const BASE_URL = 'http://localhost:3800/api/notes'
+// const BASE_URL = 'http://localhost:3800/api/notes'
+ const BASE_URL = 'https://mern-dup-1.onrender.com'
 
 export const api = axios.create({
   baseURL: BASE_URL,
